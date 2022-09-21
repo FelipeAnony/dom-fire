4ce4db8826773a994ff8ea9183fae2510fd5f323
Felipe Mendonca <felipemom2001@gmail.com>
2022-09-21 16:03:51 +0200

    chore: finish boilerplate

---
+++ src/main/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import '../styles/global.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Hello world</h1>
+  </React.StrictMode>,
+  document.getElementById('main')
+);
